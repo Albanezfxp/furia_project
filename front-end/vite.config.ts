67c38457d06c8ecaee4a27acb7868a6bf355a087
Albanezfxp <gabrielalbanez2701@gmail.com>
2025-05-04 20:07:14 -0300

    documents add

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Substitua "repository-name" pelo nome do seu repositório
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/furia_bot/',
+});
