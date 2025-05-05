0ab5de68c7ee5f413ec6a72a61c1c64060080b08
Albanezfxp <gabrielalbanez2701@gmail.com>
2025-05-04 22:07:27 -0300

    Prisma configurado para produção

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import 'module-alias/register';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
