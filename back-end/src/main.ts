565cb789991876ddfba7389ba268eb142f424721
Albanezfxp <gabrielalbanez2701@gmail.com>
2025-05-04 21:48:26 -0300

    Corrige configuração do Prisma e CORS

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   // Configuração para Vercel Serverless
   app.enableCors({
     origin: [
-      'https://furia-bot-wlvj.vercel.app/',
+      'https://furia-bot-wlvj.vercel.app',
       'http://localhost:3000'
     ], 
     credentials: true // Se usar cookies/tokens
