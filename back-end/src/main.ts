139be12d4e77cd75ca220ead9bc11e122272eaea
Albanezfxp <gabrielalbanez2701@gmail.com>
2025-05-04 21:41:49 -0300

    add vercel config

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -7,9 +7,11 @@ async function bootstrap() {
   // Configuração para Vercel Serverless
   app.enableCors({
     origin: [
-      'https://seu-front.vercel.app',
+      'https://furia-bot-wlvj.vercel.app/',
       'http://localhost:3000'
-    ]
+    ], 
+    credentials: true // Se usar cookies/tokens
+
   });
 
   await app.listen(process.env.PORT || 3000);
